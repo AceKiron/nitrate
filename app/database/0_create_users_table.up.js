5e8be2a2bc3c1b01efeb2671e4807c301f0227db
Ace <acekiron@outlook.com>
2024-09-22 13:18:35 +0200

    Update 0_create_users_table.up.js

--- app/database/0_create_users_table.up.js
+++ app/database/0_create_users_table.up.js
@@ -1,12 +1,14 @@
-Tables.createIfNotExists("users", function(table) {
-    table.id();
+module.exports = (Table) => {
+    Tables.createIfNotExists("users", function(table) {
+        table.id();
+        
+        table.string("username").unique();
+        table.string("email").unique();
     
-    table.string("username").unique();
-    table.string("email").unique();
-
-    table.timestamp("email_verified_at").nullable();
+        table.timestamp("email_verified_at").nullable();
+        
+        table.hash("password", "password_salt");
     
-    table.hash("password", "password_salt");
-
-    table.timestamps();
-});
+        table.timestamps();
+    });
+}
